034b80adc8b0c83d29879fce9f8ef5b1997c0786
Hiren S <hirens@tekion.com>
2024-04-21 11:48:23 +0530

    scroll fix + reorder prompts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { createPromptItem, postAnswer, postQuestion } from './elements';
 import { INTRO_MESSAGE, PROMPTS } from './messages.constants';
-import './style.css'
+import './styles/global.css'
 import { typeWriter } from './typeWriter';
 import { sleep } from './utils';
 import { handleExpandPromptButtonClick } from './handlers';
@@ -21,12 +21,17 @@ async function handlePromptClick(id) {
 
   const selectedPrompt = PROMPTS.find((prompt) => id === prompt.id);
   postQuestion(selectedPrompt.question);
-  await sleep(500);
+  await sleep(200);
   postAnswer(selectedPrompt.answers[0]);
+  await sleep(300);
+  const chatContainer = document.getElementById("chat-container");
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  initPrompts();
 }
 
 function initPrompts() {
   const prompt = document.getElementsByClassName("prompts")[0];
+  prompt.innerHTML = ""
   const promptList = PROMPTS.sort(() => (Math.random() * 2) - 1).map(prompt => {
     return createPromptItem(prompt, handlePromptClick);
   })
